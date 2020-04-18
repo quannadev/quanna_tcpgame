5975fdd5852cc11764db475997e8c4ad524eb01f
vutr <me@vutr.io>
2020-04-18 13:44:48 +0700

    clean up & clarify

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate amethyst;
 #[macro_use]
 extern crate log;
-#[macro_use]
+// #[macro_use]
 extern crate serde;
-#[macro_use]
+// #[macro_use]
 extern crate serde_json;
-use amethyst::Result;
+
 mod managers;
 mod network;
+
+use amethyst::Result as AmethystResult;
 use network::{Config, Networking};
 
-fn main() -> Result<()> {
-    let _run = Networking::new(Config::default());
+fn main() -> AmethystResult<()> {
+    Networking::new(Config::default());
     Ok(())
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,8 +1,10 @@
-use amethyst::{
-    core::frame_limiter::FrameRateLimitStrategy, network::simulation::tcp::TcpNetworkBundle,
-    prelude::*, utils::application_root_dir, Result,
-};
-use std::net::{SocketAddr, TcpStream};
+use amethyst::core::frame_limiter::FrameRateLimitStrategy;
+use amethyst::network::simulation::tcp::TcpNetworkBundle;
+use amethyst::utils::application_root_dir;
+// use amethyst::Result as AmethystResult;
+use amethyst::prelude::*;
+
+// use std::net::{SocketAddr, TcpStream};
 use std::time::Duration;
 
 pub mod config;
@@ -20,32 +22,36 @@ use tcp_socket::TCPSocket;
 pub struct Networking {
     pub config: Config,
 }
+
 impl Networking {
     pub fn new(config: Config) {
-        Self {
-            config: config.clone(),
-        };
+        Self { config: config.clone() };
         Self::init(config)
     }
+
     fn init(config: Config) {
-        let socket = TCPSocket::new(config.addr.as_str());
+        let socket_addr = config.addr.as_str();
+        let socket = TCPSocket::new(socket_addr);
         let assets_dir = application_root_dir().unwrap().join("./");
+
+        let tcp_bundle = TcpNetworkBundle::new(
+            Some(socket.socket),
+            config.buffer_size,
+        );
+
         let game_data = GameDataBuilder::default()
-            .with_bundle(TcpNetworkBundle::new(
-                Some(socket.socket),
-                config.buffer_size,
-            ))
+            .with_bundle(tcp_bundle)
             .unwrap()
             .with_bundle(SpamReceiveBundle)
             .unwrap();
+
+        let frame_limit = FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2));
         let mut game = Application::build(assets_dir, GameState)
             .unwrap()
-            .with_frame_limit(
-                FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
-                60,
-            )
+            .with_frame_limit(frame_limit, 60)
             .build(game_data)
             .unwrap();
+
         game.run();
     }
 }
